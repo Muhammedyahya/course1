cab82a721914350c9e388b93b9287177dc9b255b
Ashraf Abubaker <ashraf.m.alraheem@gmail.com>
2018-11-10 23:40:16 +0300

    main.c updated to call course1() if the compile time switch is passed

--- src/main.c
+++ src/main.c
@@ -10,45 +10,19 @@
  *****************************************************************************/
 /**
  * @file main.c
- * @brief Main entry point to the C1M2 Assessment
- *
- * This file contains the main code for the C1M2 assesment. Students
- * are not to change any of the code, they are instead supposed to compile
- * these files with their makefile.
- *
- * @author Alex Fosdick
- * @date April 1 2017
- *
+ * @brief main call course1 function if COURSE1 is passed at compile time
+ * This file contains the main code for the course1 assesment
+ * Modified By Ashraf Abubaker Mohammed for Embededd Software Course 
+ * @date Nov  10 2018
  */
-#include "platform.h"
-#include "memory.h"
 
-#define MAX_LENGTH (10)
-char buffer[MAX_LENGTH];
+#include "course1.c"
 
-/* A pretty boring main file */
+/* A pretty short main file */
 int main(void) {
-  unsigned int i;
-  char value;
-
-  /* Code below does some arbitrary memory Reads & writes */
-  clear_all(buffer, MAX_LENGTH);
-  set_all( ( buffer + 8 ), 43, 2); 
-  set_value(buffer, 0, 0x61);
-  value = get_value(buffer, 9);
-  set_value(buffer, 9, (value + 0x27));
-  set_value(buffer, 3, 0x37);
-  set_value(buffer, 1, 88);
-  set_value(buffer, 4, '2');
-  value = get_value(buffer, 1);
-  set_value(buffer, 2, 121);
-  set_value(buffer, 7, (value - 12));
-  set_value(buffer, 5, 0x5F);
-
-  for ( i = 0; i < MAX_LENGTH; i++ ){
-    PRINTF("%c", buffer[i]);
-  }
-  PRINTF("\n");
+  #ifdef COURSE1   // Use compile time switch to execute this function
+	course1();
+  #endif
   return 0;
 }
 
