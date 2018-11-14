d2ee9013fdf5c7030f9e1ce6040a836bb2b2944f
Ashraf Abubaker <ashraf.m.alraheem@gmail.com>
2018-11-14 22:04:06 +0200

    Making some programing in Integer to ASCII conversion

--- include/common/data.h
+++ include/common/data.h
@@ -20,6 +20,29 @@
  */
  #ifndef __DATA_H__
  #define __DATA_H__
- 
- 
+ /**
+ * @brief Integer to ASCII Conversion Function 
+ *
+ * This function will take singed integer number and converts it to ASCII with defined 
+ * base. 
+ * It saves the converted data  in 8 bit memory locations assigned by pointer. 
+ * The string of data is terminated by NULL.
+ * It returns the length of data converted including the terminator\
+ * 
+ * Algorithm: 
+ * 1- Check if the number is negative: 
+ * 		-if it is negative the first byte is negative sign.
+ * 2- Convert the integer into signle decimal digits and store them in memory 
+ * 		Most significant digit should be stored first.
+ * 3- Change the data into ASCII with base 10. 
+ * 4- If the base is not 10 convert to the correct.
+ * 5- Return the length.
+ * 
+ * @param data integer data for conversion
+ * @param ptr Pointer to memory location where result of conversion is saved
+ * @param base of converted data from 2 to 16. 
+ *
+ * @return void.
+ */
+ uint8_t my_itoa(int32_t data, uint8_t * ptr, uint32_t base);
  #endif /*DATA.H*/
--- src/data.c
+++ src/data.c
@@ -24,3 +24,41 @@
  Function Definitions
 ***********************************************************/
 
+uint8_t my_itoa(int32_t data, uint8_t * ptr, uint32_t base){
+	uint8_t *Start_Address = ptr;
+	uint8_t length=1;
+	uint8_t temp_length;
+	if(data<0){ // If the data is negative the MSD is the negative sign
+		*ptr = 45; // Decimal equivelant of "-" sign
+		ptr++;
+		data=data*-1; // Convert the data into positive
+		length++;
+	}
+	while(length<=32) // Dismantle the number into individual digits by dividing by 10;
+	{                 // The most significant digit will be at the last location.
+		
+		*ptr = data%10;  // Store the modulus
+		data = data/10;  // Shift the data to the right
+		*ptr = *ptr + 48; // Convert to the decimal ASCII equivelant.
+		ptr++;            // point to the next location
+		length++;    
+		if((data==0)){ // The number division is completed
+			*ptr=NULL; // Terminate by NULL
+			break; 
+		}
+	}
+	temp_length = length;
+	if(*Start_Address==45){ // If the number is negative point to the next location
+		ptr = Start_Address+1;
+		temp_length = temp_length -2;
+	}else{
+		ptr = Start_Address;
+		temp_length = temp_length -1;
+	}
+	ptr = my_reverse(ptr, temp_length); // Reverse the order so That the first location 
+										// Points to the most significant digit (MSD)
+	
+	
+	
+	return length;
+}
