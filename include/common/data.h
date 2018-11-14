065508b12ec11abb05be42508551b995daa846f0
Ashraf Abubaker <ashraf.m.alraheem@gmail.com>
2018-11-14 19:53:00 +0200

    Start The data manipulation source files

---
+++ include/common/data.h
@@ -0,0 +1,25 @@
+/******************************************************************************
+ * Copyright (C) 2018 by Ashraf Abdalraheem
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Ashraf Abdalraheem are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file data.c
+ * @brief Basic Data Manipulation
+ *
+ * This Header file provides a basic manipulation of data
+ *
+ * @author Ashraf Abdalraheem
+ * @date Nov 14 2018
+ *
+ */
+ #ifndef __DATA_H__
+ #define __DATA_H__
+ 
+ 
+ #endif /*DATA.H*/
